c8cb35a94f43333b3f2dc968d0fceab38a2855bc
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-06-01 02:39:14 +0530

    feat: implementing pwa

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,4 +13,8 @@ root.render(
   </ChakraProvider>
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 reportWebVitals();
